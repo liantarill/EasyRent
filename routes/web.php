ceb89f1b45fbf491226d5ff3113c8fc3cbaaf829
liantarill <yusufherliananantaril@gmail.com>
2025-11-15 02:05:08 +0700

    feature rents v1

--- routes/web.php
+++ routes/web.php
@@ -50,4 +50,5 @@ Route::middleware(['auth', 'role:customer'])->name('customer.')->group(function 
 Route::middleware(['auth', 'role:admin'])->prefix('admin')->name('admin.')->group(function () {
     Route::get('/dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('dashboard');
     // tambahkan route admin lain di sini...
+    Route::resource('rents', App\Http\Controllers\Admin\RentController::class);
 });
